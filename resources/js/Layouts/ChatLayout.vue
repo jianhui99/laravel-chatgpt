5e1600770326fa97531230754add9572ff05bf87
Jian <jianhui9998@gmail.com>
2023-03-26 21:32:30 +0800

    feat: add chat page

---
+++ resources/js/Layouts/ChatLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <aside class="ml-[-100%] fixed z-10 top-0 pb-3 px-4 w-full flex flex-col justify-between h-screen border-r dark:bg-gray-900 transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
+        <div>
+            <slot name="aside" />
+        </div>
+    </aside>
+    <div class="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
+        <div class="pt-6">
+            <main>
+                <slot />
+            </main>
+        </div>
+        <div class="fixed z-20 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] bottom-0 h-28 dark:bg-gray-800 bg-opacity-70 shadow-2xl lg:py-2.5">
+            <slot name="form" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ resources/js/Pages/Chat/ChatIndex.vue
@@ -0,0 +1,32 @@
+<template>
+    <AuthenticatedLayout>
+        <ChatLayout>
+            <template #aside>
+
+            </template>
+            <div class="w-full flex text-white">
+
+            </div>
+            <template #form>
+                <section class="px-6 top-0 pt-8">
+                    <div class="w-full">
+                        <div class="relative flex-1 flex items-center">
+                            <input
+                                type="text"
+                                class="w-full dark:bg-gray-700 text-white rounded-lg"
+                                placeholder="Ask Laravel AI"
+                                ref="promtInput"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </template>
+        </ChatLayout>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import ChatLayout from '@/Layouts/ChatLayout.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+</script>
